8ffc219abb62423eaff55e4b84c7d2d9d169c1f7
fttkm77@gmail.com <fttkm77@gmail.com>
2023-04-13 00:24:12 +0900

    edit user migrationfile

--- simple-recipe-store/database/migrations/2014_10_12_000000_create_users_table.php
+++ simple-recipe-store/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedInteger('age');
+            $table->unsignedInteger('age')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
