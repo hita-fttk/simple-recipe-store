d9496936cbb77d92aa019885b577a6af045a5816
fttkm77@gmail.com <fttkm77@gmail.com>
2023-04-19 23:18:19 +0900

    create recipe&ingredient route/view/dashboard

--- simple-recipe-store/routes/web.php
+++ simple-recipe-store/routes/web.php
@@ -22,6 +22,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/recipe', function () {
+    return view('recipe');
+})->middleware(['auth', 'verified'])->name('recipe');
+
+Route::get('/ingredient', function () {
+    return view('ingredient');
+})->middleware(['auth', 'verified'])->name('ingredient');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
