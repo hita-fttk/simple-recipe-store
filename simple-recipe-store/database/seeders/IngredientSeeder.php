788c21f33208b15782f70edb3e2a6144942b02de
fttkm77@gmail.com <fttkm77@gmail.com>
2023-04-24 22:06:31 +0900

    create ingredientfactory rezister record to table

--- simple-recipe-store/database/seeders/IngredientSeeder.php
+++ simple-recipe-store/database/seeders/IngredientSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Ingredient;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class IngredientSeeder extends Seeder
 {
@@ -16,8 +18,8 @@ class IngredientSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('ingredients')->insert([
-            'name' => Str::random(50),
-        ]);
+        Ingredient::factory()
+                        ->count(50)
+                        ->create();
     }
 }
